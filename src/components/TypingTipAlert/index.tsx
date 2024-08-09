4a6e17bf6d6421f3463d3412b1301d49b8053537
october-x <2290512627@qq.com>
2024-08-09 22:23:03 +0800

    feat： 修改主题为蓝色

--- src/components/TypingTipAlert/index.tsx
+++ src/components/TypingTipAlert/index.tsx
@@ -4,11 +4,11 @@ import {Badge} from "@/components/ui/badge";
 
 const TypingTipAlert = () => {
     return (
-        <Alert className='absolute top-5 w-80 mt-10 animate-bounce text-lg'>
+        <Alert className='absolute top-5 w-fit mt-10 animate-bounce text-lg'>
             <AlertDescription className='text-lg'>
-                Press <Badge
-                className='bg-gray-100 text-primary text-lg font-light'>Enter</Badge> to
-                start typing
+                Press
+                <Badge className='bg-gray-100 text-primary text-lg font-light'>Enter</Badge>
+                to start typing
             </AlertDescription>
         </Alert>
     );
